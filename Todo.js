635c8cacabd15c034f71312c5f8f14d1f296f11c
SideRea1 <zvezdilin.sasha2017@yandex.ru>
2021-10-31 01:11:36 +0300

    changes

--- Todo.js
+++ Todo.js
@@ -1,5 +1,6 @@
 document.querySelector('#push').onclick = function () {
-    if (document.querySelector('#newtask input').value.length === 0) {
+    const value = document.querySelector('#newtask input').value
+    if (value === 0) {
         alert("Please Enter a Task")
     } else {
         document.querySelector('#tasks').innerHTML
@@ -7,24 +8,23 @@ document.querySelector('#push').onclick = function () {
         <div class = "content">
         <div class="task">
            <span id="taskname">
-           ${document.querySelector('#newtask input').value}
+           ${value}
            </span>
            <button class="delete">
            <i class="fas fa-backspace"></i>
            </button>
            </div>
            </div>
-        `;
+        `; 
+        addTask(value)
     }
-
-    let current_tasks = document.querySelectorAll(".delete");
-    for (let i = 0; i < current_tasks.length; i++) {
-        current_tasks[i].onclick = function () {
-            this.parentNode.remove();
-        }
-    }
-
-    document.querySelector("#newtask input").value = "";
 }
 
 
+let current_tasks = document.querySelectorAll(".delete");
+for (let i = 0; i < current_tasks.length; i++) {
+    current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+        // document.querySelector("#newtask input").value = "";
+    }
+}
--- axios/axios_req.js
+++ axios/axios_req.js
@@ -1,10 +1,27 @@
-const sendGetRequest = async () => {
-    let response = await fetch('http://localhost:8080/user');
+const getTasks = async () => {
+    let response = await fetch('http://localhost:8080/api/user');
 
     if (response.ok) { // если HTTP-статус в диапазоне 200-299
         // получаем тело ответа (см. про этот метод ниже)
         let json = await response.json();
+        console.log(json);
     } else {
         alert("Ошибка HTTP: " + response.status);
     }
 };
+
+const removeTask = async () => {
+    fetch('http://localhost:8080/api/user/:id', {
+  method: 'DELETE',
+})
+.then(res => res.text()) // or res.json()
+.then(res => console.log(res))
+};
+
+
+const addTask = async (value) => {
+    fetch('http://localhost:8080/api/user', {
+  method: 'POST', body: value
+})
+};
+
